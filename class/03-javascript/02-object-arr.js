d0cb82a6bbe76dcbf800f612958221da6eb1301e
roze <wonder_0128@naver.com>
2024-05-07 15:29:00 +0900

    0507

---
+++ class/03-javascript/02-object-arr.js
@@ -0,0 +1,26 @@
+const fruits = [
+  {number: 1, title: '레드향'},
+  {number: 2, title: '샤인머스캣'},
+  {number: 3, title: '산청딸기'},
+  {number: 4, title: '한라봉'},
+  {number: 5, title: '사과'},
+  {number: 6, title: '애플망고'},
+  {number: 7, title: '딸기'},
+  {number: 8, title: '천혜향'},
+  {number: 9, title: '과일선물세트'},
+  {number: 10, title: '귤'}
+]
+// undefined
+for(let i = 0; i < fruits.length; i++){
+  console.log(fruits[i].number + ' ' + fruits[i].title);
+}
+// 1 레드향
+// 2 샤인머스캣
+// 3 산청딸기
+// 4 한라봉
+// 5 사과
+// 6 애플망고
+// 7 딸기
+// 8 천혜향
+// 9 과일선물세트
+// 10 귤
---
+++ class/03-javascript/연습-03-object.js
@@ -0,0 +1,16 @@
+let friend = {
+  name: '철수', 
+  age: 13, 
+  camp: 'codeCamp'
+}
+// undefined
+friend
+// {name: '철수', age: 13, camp: 'codeCamp'}
+friend.name
+// '철수'
+friend.age
+// 13
+friend.camp
+// 'codeCamp'
+friend.test
+// undefined
