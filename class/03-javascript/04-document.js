19505304ecda8b60557c23b581644406ccc589a9
roze <wonder_0128@naver.com>
2024-05-07 17:11:00 +0900

    0507

---
+++ class/03-javascript/04-document.js
@@ -0,0 +1,5 @@
+function changeText() {
+  document.getElementById('target').innerText = 'World';
+  document.querySelector('input').value = 'Hello'
+  // document.querySelector('div').innerText = 'World!!';
+}
