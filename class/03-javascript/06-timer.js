d5d473719f177e7fe11264f56ec8c385b1d9fb6a
roze <wonder_0128@naver.com>
2024-05-23 09:57:39 +0900

    0523

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,21 +1,33 @@
+let isStarted = false;
 const randomNumber = () => {
-  const number = String(Math.floor(Math.random() * 1000000)).padStart(6, '0');
-  const target = document.getElementById('result__number');
-  target.innerText = number;
-  target.style.color = `#${number}`;
-  
-  let time = 180;
-  setInterval(function() {
-    if(time >= 0){
-      let minute = Math.trunc(time / 60);
-      let second = String(time % 60).padStart(2, '0');
-      document.getElementById('timer').innerText = `${minute}:${second}`
-      time--;
-    } else {
-      document.querySelector('.chk').disabled = true;
-    }
-  }, 100)
 
+  if(isStarted === false){
+    // 타이머가 작동중이 아닐 때
+    isStarted = true;
+    document.querySelector('.chk').disabled = false;
+
+    const number = String(Math.floor(Math.random() * 1000000)).padStart(6, '0');
+    const target = document.getElementById('result__number');
+    target.innerText = number;
+    target.style.color = `#${number}`;
+    
+    let time = 10;
+    let timer = setInterval(function() {
+      if(time >= 0){
+        let minute = Math.trunc(time / 60);
+        let second = String(time % 60).padStart(2, '0');
+        document.getElementById('timer').innerText = `${minute}:${second}`
+        time--;
+      } else {
+        document.querySelector('.chk').disabled = true;
+        isStarted = false;
+        clearInterval(timer);
+      }
+    }, 100)
+    
+  } else {
+    // 타이머가 작동중일 때
+  }
 }
 
 
